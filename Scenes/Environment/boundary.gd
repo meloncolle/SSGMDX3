7f6ecbcd2b47183786bba30156ad0766181daef1
meloncolle <melon@meloncolle.com>
2024-11-25 18:44:18 -0800

    add evil boundary that kills u

--- Scenes/Environment/boundary.gd
+++ Scenes/Environment/boundary.gd
@@ -1,8 +1,9 @@
 @tool
 extends Line2D
 
-@onready var collider: CollisionPolygon2D = $StaticBody2D/CollisionPolygon2D
-@onready var staticBody: StaticBody2D = $StaticBody2D
+@onready var collider: CollisionPolygon2D = $Body2D/CollisionPolygon2D
+@onready var staticBody: Node2D = $Body2D
+@onready var fill: Polygon2D = $Polygon2D
 @export_range(0, 100, 0.1) var bounce: float = 0:
 	set = set_bounce
 
@@ -12,7 +13,7 @@ func _ready():
 
 func set_bounce(value: float):
 	bounce = value
-	if staticBody != null:
+	if staticBody != null && staticBody is StaticBody2D:
 		staticBody.physics_material_override.bounce = value
 
 func _set(name, value):
@@ -20,3 +21,9 @@ func _set(name, value):
 		"points":
 			if collider != null:
 				collider.polygon = value
+				fill.polygon = value
+
+# temp for death boundary only
+func _on_entered(body: Node2D) -> void:
+	if body.is_in_group("balls"):
+		body.destroy()
